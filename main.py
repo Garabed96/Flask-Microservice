2f125bfc3e5d1d3ff88e7cac86ba412c508fe47d
Garo <garonazarian09@gmail.com>
2023-09-14 16:45:41 +0700

    refactoring models.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from random import choice
 from flask import jsonify
 from flask_cors import CORS
-
+from models import db, User, Cafe
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -11,57 +10,9 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 ##Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
-
-## User Table configuration
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(250), unique=True, nullable=False)
-    name = db.Column(db.String(250), nullable=False)
-    password = db.Column(db.String(250), nullable=False)
-    membership_status = db.Column(db.VARCHAR(10), nullable=False) # "basic", "premium", "admin"
-    # weight should be either kg or lbs
-    weight_value = db.Column(db.Float, nullable=False)
-    weight_unit = db.Column(db.VARCHAR(5), nullable=False)
-    height_value = db.Column(db.Float, nullable=False)
-    height_unit = db.Column(db.VARCHAR(5), nullable=False)
-
-
-    def to_dict(self):
-        # I'm returning a dictionary representation of the User object.
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
-        # dict = {}
-        # # Loop through each column in the data record
-        # for column in self.__table__.columns:
-        #     dict[column.name] = getattr(self, column.name)
-        # return dict
-
-
-##Cafe TABLE Configuration
-class Cafe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), unique=True, nullable=False)
-    map_url = db.Column(db.String(500), nullable=False)
-    img_url = db.Column(db.String(500), nullable=False)
-    location = db.Column(db.String(250), nullable=False)
-    seats = db.Column(db.String(250), nullable=False)
-    has_toilet = db.Column(db.Boolean, nullable=False)
-    has_wifi = db.Column(db.Boolean, nullable=False)
-    has_sockets = db.Column(db.Boolean, nullable=False)
-    can_take_calls = db.Column(db.Boolean, nullable=False)
-    coffee_price = db.Column(db.String(250), nullable=True)
-
-    def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
-        # dict = {}
-        # # Loop through each column in the data record
-        # for column in self.__table__.columns:
-        #     dict[column.name] = getattr(self, column.name)
-        # return dict
-
-
 
 with app.app_context():
+    db.init_app(app)
     db.create_all()
 
 @app.route("/")
---
+++ models.py
@@ -0,0 +1,48 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+## User Table configuration
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    membership_status = db.Column(db.VARCHAR(10), nullable=False) # "basic", "premium", "admin"
+    # weight should be either kg or lbs
+    weight_value = db.Column(db.Float, nullable=False)
+    weight_unit = db.Column(db.VARCHAR(5), nullable=False)
+    height_value = db.Column(db.Float, nullable=False)
+    height_unit = db.Column(db.VARCHAR(5), nullable=False)
+
+    def to_dict(self):
+        # I'm returning a dictionary representation of the User object.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        # dict = {}
+        # # Loop through each column in the data record
+        # for column in self.__table__.columns:
+        #     dict[column.name] = getattr(self, column.name)
+        # return dict
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        # dict = {}
+        # # Loop through each column in the data record
+        # for column in self.__table__.columns:
+        #     dict[column.name] = getattr(self, column.name)
+        # return dict

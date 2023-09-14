0587fe22153a5ac6c6246cae2c52c44005306c74
Garo <garonazarian09@gmail.com>
2023-09-14 16:26:29 +0700

    adding vite files

--- pytest/api_get_test.py
+++ pytest/api_get_test.py
@@ -1,43 +1,35 @@
 import requests
+import json
 
-ENDPOINT = ' http://127.0.0.1:8000/random_user'
-UPDATE = 'http://127.0.0.1:8000/update'
-POST = 'http://127.0.0.1:8000/adduser'
-SPECIFIC_USER = 'http://127.0.0.1:8000/user'
+SHOW_ALL = 'http://127.0.0.1:8000/all'
+GET_RANDOM_USER = ' http://127.0.0.1:8000/random_user'
+UPDATE_USERS_NAME = 'http://127.0.0.1:8000/update'
+CREATE_USER = 'http://127.0.0.1:8000/adduser'
+GET_SPECIFIC_USER = 'http://127.0.0.1:8000/user'
+DELETE_SPECIFIC_USER = 'http://127.0.0.1:8000/delete'
+
+# just checking if endpoint working, useless test we can just check in http-client
 def test_can_call_endpoint():
-    response = requests.get(ENDPOINT)
+    response = requests.get(GET_RANDOM_USER)
     assert response.status_code == 200
 
+# test if we can get all users
+def test_can_get_all_users():
+    response = requests.get(SHOW_ALL)
+    assert response.status_code == 200
 
 # This test fails if the exact payload exists in the database
-def test_cal_create_new_user():
-    payload={
-        "name": "Garo Doom Goblin",
-        "email": "DOOGoblinM@email.com",
-        "password": "010101010",
-        "membership_status": "DOOM",
-        "weight_value": "130",
-        "weight_unit": "kg",
-        "height_value": "184",
-        "height_unit": "cm"
-    }
-    response = requests.post(POST, json=payload)
-    assert response.status_code == 201
-
-    data = response.json()
-    print(data)
-
-
-# lets now get a user based off of their id number
-def test_can_get_user_by_id():
-    response = requests.get(SPECIFIC_USER + '/1'
-    assert response.status_code == 200
-
-
-def get_specific_user(user_id):
-    return requests.get(SPECIFIC_USER + f'/{user_id}')
-
-def update_user_info(user_id, new_name):
-    return requests.patch(UPDATE + f'/{user_id}', json={"name": new_name})
+# def test_cal_create_new_user():
+#     payload={
+#         "name": "Garo Doom Goblin",
+#         "email": "DOOGoblinM@email.com",
+#         "password": "010101010",
+#         "membership_status": "DOOM",
+#         "weight_value": "130",
+#         "weight_unit": "kg",
+#         "height_value": "184",
+#         "height_unit": "cm"
+#     }
+#     pass
 
 
